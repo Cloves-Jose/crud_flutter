d9b899313af7dc82523a86c0f99d484b29783316
Cloves Jose <clovesjsilva8@gmail.com>
2021-07-15 16:08:38 -0300

    users, user_list, main

--- flutter_crud/lib/main.dart
+++ flutter_crud/lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/provider/users.dart';
 import 'package:flutter_crud/view/user_list.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: UserList(),
-    );
+    return ChangeNotifierProvider(
+        create: (ctx) => Users(),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: UserList(),
+        ));
   }
 }
