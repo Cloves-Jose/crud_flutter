40f06d138f0bf9dc2028c2364ff30646db4bb36d
Cloves Jose <clovesjsilva8@gmail.com>
2021-07-15 14:31:42 -0300

    models e data

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl: '',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Cláudio',
+    email: 'claudio@alunos.com.br',
+    avatarUrl: '',
+  ),
+  '3': const User(
+    id: '1',
+    name: 'João',
+    email: 'joao@alunos.com.br',
+    avatarUrl: '',
+  )
+};
